ddd7af01ac560b41bec237e41d70f1270bda3a8b
HamzaMushtaq23 <hamzakgan.32@gmail.com>
2026-09-15 15:36:17 +0500

    Update Terraform backend authentication

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "tfstateazureaks01"
     container_name       = "tfstate"
     key                  = "azure-aks.tfstate"
+    use_cli              = true
   }
 }
